6fb652d57c886c9931df91bd101c16ad6699fc22
Kat Zdzienkowska <katzdzienkowska@kats-mbp.home>
2022-06-27 19:10:12 +0100

    mvp and extensions done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] = useState([
+    {name: 'Grocery shopping', priority: 'high'},
+    {name: 'Clean bathroom', priority: 'low'},
+    {name: `Car's MOT`, priority: 'high'}
+  ]);
+
+  const [newTodo, setNewTodo] = useState('');
+
+  const [todoPriority, setTodoPriority] = useState('');
+
+  const todosList = todos.map((todo, index) => {
+    return (
+      <li key={index} className={todo.priority === 'high' ? 'high-priority' : 'low-priority'}>
+        <span>{todo.name}</span>
+      </li>
+    );
+  });
+
+  const handleTodoInput = (evt) => {
+    setNewTodo(evt.target.value);
+  };
+
+  const handleTodoPriority = (evt) => {
+    setTodoPriority(evt.target.value);
+  };
+
+  const saveNewTodo = (evt) => {
+    evt.preventDefault();
+    const copyTodos = [...todos];
+    copyTodos.push({name: newTodo, priority: todoPriority});
+    setTodos(copyTodos);
+    setNewTodo('');
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='todo-list'>
+
+      <h1>Todo List</h1>
+      
+      <form onSubmit={saveNewTodo}>
+        <label htmlFor='new-todo'></label>
+        <input id='new-item' type='text' value={newTodo} onChange={handleTodoInput}/>
+        <label htmlFor='high'>High</label>
+        <input id='high' type='radio' name='priority' value='high' onChange={handleTodoPriority}/>
+        <label htmlFor='low'>Low</label>
+        <input id='low' type='radio' name='priority' value='low' onChange={handleTodoPriority}/>
+        <input type='submit' value='Save Item'/>
+      </form>
+
+      <ul>
+        {todosList}
+      </ul>
+
     </div>
+
   );
 }
 
